fd8bf44aa67fe4dc374f3a539fa61f64b473f99b
Amruta1990 <amrutamnikam@gmail.com>
2023-04-24 18:36:09 +0530

    Application properties in different enviorment

---
+++ src/main/java/com/happytech/electronicstore/dtos/BaseEntityDto.java
@@ -0,0 +1,12 @@
+package com.happytech.electronicstore.dtos;
+
+import java.time.LocalDate;
+
+public class BaseEntityDto {
+
+    private LocalDate createdate;
+
+    private  LocalDate updatedate;
+
+    private String Isactive;
+}
