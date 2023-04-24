dc8747563838608faadcf03437985cfa71bdd454
Amruta1990 <amrutamnikam@gmail.com>
2023-04-24 18:44:43 +0530

    ApiResponse class is created

---
+++ src/main/java/com/happytech/electronicstore/dtos/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.happytech.electronicstore.dtos;
+
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
+@Builder
+public class ApiResponse {
+
+    private String message;
+
+    private boolean success;
+
+    private HttpStatus status;
+}
